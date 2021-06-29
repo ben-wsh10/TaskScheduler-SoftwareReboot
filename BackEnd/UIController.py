f809b7a2c18891966755813e535f6c018d53a7ce
aerris <benwu1997@gmail.com>
2021-06-29 13:05:52 +0800

    set dropdown menu for period (UI)

--- BackEnd/UIController.py
+++ BackEnd/UIController.py
@@ -7,6 +7,24 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 taskType, taskPeriod, taskName, taskPath, taskTime = "", "", "", "", ""
+dailyList = ["-"]
+weekList = ["Select a Day", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
+# weekListConversion = ["MON", "TUES", "WED", "THU", "FRI", "SAT", "SUN"]
+monthList = [   "Select a date",
+                "1st Day of the Month", "2nd Day of the Month", "3rd Day of the Month",
+                "4th Day of the Month", "5th Day of the Month", "6th Day of the Month",
+                "7th Day of the Month", "8th Day of the Month", "9th Day of the Month",
+                "10th Day of the Month", "11th Day of the Month", "12th Day of the Month",
+                "13th Day of the Month", "14th Day of the Month", "15th Day of the Month",
+                "16th Day of the Month", "17th Day of the Month", "18th Day of the Month",
+                "19th Day of the Month", "20th Day of the Month", "21st Day of the Month",
+                "22nd Day of the Month", "23rd Day of the Month", "24th Day of the Month",
+                "25th Day of the Month", "26h Day of the Month", "27th Day of the Month",
+                "28th Day of the Month", "29th Day of the Month", "30th Day of the Month",
+                "31st Day of the Month",
+            ]
+# monthListConversion = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+#                        27, 28, 29, 30, 31]
 
 
 def startLogging():
@@ -18,11 +36,10 @@ def startLogging():
 
 def createTask():
     try:
-        cmdLine = 'SCHTASKS /{taskType} /SC {taskPeriod} /TN {taskName} /TR {taskPath} /ST {taskTime}'\
-                .format(taskType=taskType, taskPeriod="", taskName="", taskPath="", taskTime="")
+        cmdLine = 'SCHTASKS /{taskType} /SC {taskPeriod} /TN {taskName} /TR {taskPath} /ST {taskTime}' \
+            .format(taskType=taskType, taskPeriod="", taskName="", taskPath="", taskTime="")
         # subprocess.call(['start', 'cmd', '/k', cmdLine], shell=True)
         # __logger.info("Successfully written scheduled task")
     except Exception as e:
         print(e)
         logger.exception("Unable to create scheduled task")
-

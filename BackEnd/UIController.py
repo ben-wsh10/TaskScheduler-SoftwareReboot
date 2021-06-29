7ccea1281d769faaf66f9fcac0ce2b66455dff44
aerris <benwu1997@gmail.com>
2021-06-29 15:16:36 +0800

    update cmdLine Label to realtime

--- BackEnd/UIController.py
+++ BackEnd/UIController.py
@@ -7,22 +7,25 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 taskType, taskPeriod, taskName, taskPath, taskTime = "", "", "", "", ""
+cmdline = ""
 dailyList = ["-"]
 weekList = ["Select a Day", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
 # weekListConversion = ["MON", "TUES", "WED", "THU", "FRI", "SAT", "SUN"]
-monthList = [   "Select a date",
-                "1st Day of the Month", "2nd Day of the Month", "3rd Day of the Month",
-                "4th Day of the Month", "5th Day of the Month", "6th Day of the Month",
-                "7th Day of the Month", "8th Day of the Month", "9th Day of the Month",
-                "10th Day of the Month", "11th Day of the Month", "12th Day of the Month",
-                "13th Day of the Month", "14th Day of the Month", "15th Day of the Month",
-                "16th Day of the Month", "17th Day of the Month", "18th Day of the Month",
-                "19th Day of the Month", "20th Day of the Month", "21st Day of the Month",
-                "22nd Day of the Month", "23rd Day of the Month", "24th Day of the Month",
-                "25th Day of the Month", "26h Day of the Month", "27th Day of the Month",
-                "28th Day of the Month", "29th Day of the Month", "30th Day of the Month",
-                "31st Day of the Month",
-            ]
+monthList = ["Select a date",
+             "1st Day of the Month", "2nd Day of the Month", "3rd Day of the Month",
+             "4th Day of the Month", "5th Day of the Month", "6th Day of the Month",
+             "7th Day of the Month", "8th Day of the Month", "9th Day of the Month",
+             "10th Day of the Month", "11th Day of the Month", "12th Day of the Month",
+             "13th Day of the Month", "14th Day of the Month", "15th Day of the Month",
+             "16th Day of the Month", "17th Day of the Month", "18th Day of the Month",
+             "19th Day of the Month", "20th Day of the Month", "21st Day of the Month",
+             "22nd Day of the Month", "23rd Day of the Month", "24th Day of the Month",
+             "25th Day of the Month", "26h Day of the Month", "27th Day of the Month",
+             "28th Day of the Month", "29th Day of the Month", "30th Day of the Month",
+             "31st Day of the Month",
+             ]
+
+
 # monthListConversion = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
 #                        27, 28, 29, 30, 31]
 
@@ -34,10 +37,22 @@ def startLogging():
     logger.addHandler(fileHandler)
 
 
+def updateCmdLine():
+    global cmdline
+
+    cmdLine = 'SCHTASKS /{taskType} /SC {taskPeriod} /TN {taskName} /TR {taskPath} /ST {taskTime}' \
+        .format(taskType=taskType, taskPeriod=taskPeriod, taskName=taskName, taskPath=taskPath, taskTime=taskTime)
+
+    return cmdLine
+
+
 def createTask():
+    global cmdline
+
     try:
         cmdLine = 'SCHTASKS /{taskType} /SC {taskPeriod} /TN {taskName} /TR {taskPath} /ST {taskTime}' \
-            .format(taskType=taskType, taskPeriod="", taskName="", taskPath="", taskTime="")
+            .format(taskType=taskType, taskPeriod=taskPeriod, taskName=taskName, taskPath=taskPath, taskTime=taskTime)
+        print(cmdLine)
         # subprocess.call(['start', 'cmd', '/k', cmdLine], shell=True)
         # __logger.info("Successfully written scheduled task")
     except Exception as e:

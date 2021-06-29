0633d6f7d01f9bcaf3c38398417a9d804d2c0b67
aerris <benwu1997@gmail.com>
2021-06-29 11:44:28 +0800

    refactor UIController.py

--- BackEnd/UIController.py
+++ BackEnd/UIController.py
@@ -1,19 +1,28 @@
 import subprocess
 import logging
 
-# Logging information
+import FrontEnd.UserInterface as UI
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(levelname)s : %(asctime)s : %(message)s')
-fileHandler = logging.FileHandler('LogFile.log')
-fileHandler.setFormatter(formatter)
-logger.addHandler(fileHandler)
 
-try:
-    test = 'SCHTASKS /CREATE /SC DAILY /TN {0} /TR {1} /ST 15:38'.format("task", "C:Users\\benwu\\Desktop\\FinalYearProject-master\\Main.exe")
-    # maintenanceReboot = 'shutdown /r /t 5 /c "Scheduled Maintenance"'
-    subprocess.call(['start', 'cmd', '/k', test], shell=True)
-    logger.info("Successfully written scheduled task")
-except Exception as e:
-    print(e)
-    logger.exception("Unable to create scheduled task")
+taskType, taskPeriod, taskName, taskPath, taskTime = "", "", "", "", ""
+
+
+def startLogging():
+    formatter = logging.Formatter('%(levelname)s : %(asctime)s : %(message)s')
+    fileHandler = logging.FileHandler('LogFile.log')
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+
+
+def createTask():
+    try:
+        cmdLine = 'SCHTASKS /{taskType} /SC {taskPeriod} /TN {taskName} /TR {taskPath} /ST {taskTime}'\
+                .format(taskType=taskType, taskPeriod="", taskName="", taskPath="", taskTime="")
+        # subprocess.call(['start', 'cmd', '/k', cmdLine], shell=True)
+        # __logger.info("Successfully written scheduled task")
+    except Exception as e:
+        print(e)
+        logger.exception("Unable to create scheduled task")
+
